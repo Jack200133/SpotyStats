120d59e8149bbb8b2c538b6e15aa637c82ded320
JuanDiegoAvila <juanavila20027@gmail.com>
2023-01-25 09:49:40 -0600

    Correccion de NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,15 @@ import logo from "../assets/Logos/Spotify_Logo_RGB_White.png";
 
 const NavBar = () => {
     return (
-        <>
+        <div className="nav-bar-container">
             <img src={logo} className='logo' />
+            <h1>STATS</h1>
             <nav className="nav-bar">
-                <a to="/">HOME</a>
-                <a to="/about">ABOUT</a>
+                <a to="/">ARTISTA</a>
+                <a to="/">REGIONES</a>
+                <a to="/about">GÉNEROS</a>
             </nav>
-        </>
+        </div>
     );
 }
 
